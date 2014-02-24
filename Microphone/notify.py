8f20ff26f359c0233044956a1fa24ecb0ed85a33
BmanDesignsCanada <bman1559@gmail.com>
2014-02-23 20:20:42 -0600

    Fixed #25

--- Microphone/notify.py
+++ Microphone/notify.py
@@ -47,46 +47,26 @@ def msg_handler(*args,**keywords):
     global last_signal
     try:
         last_signal=int(keywords['path'].split("/")[4])
-        print keywords['path'], last_signal #debug
+        #print keywords['path'], last_signal #debug
+        if last_signal==SIG_DONE:
+            os.system("touch in_grey")
+        if last_signal==SIG_RECORD:
+            os.system("touch in_green")
+        if last_signal==SIG_STOP:
+            os.system("touch in_red")
+        if last_signal==SIG_WAIT:
+            os.system("touch in_progress")
     except:
         pass
 
 bus.add_signal_receiver(handler_function=msg_handler, dbus_interface='com.bmandesigns.lispeak', interface_keyword='iface',  member_keyword='member', path_keyword='path')
 
-
-# create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if len(attrs) > 0:
             self.data[tag] = {}
             for a in attrs:
                 self.data[tag][a[0]] = a[1] 
-
-class ShapedWindow(gtk.Window):
-  def __init__(self):
-      gtk.Window.__init__(self)
-      self.connect('size-allocate', self._on_size_allocate)
-      self.set_decorated(False)
-
-  def _on_size_allocate(self, win, allocation):
-      w,h = allocation.width, allocation.height
-      bitmap = gtk.gdk.Pixmap(None, w, h, 1)
-      cr = bitmap.cairo_create()
-
-      # Clear the bitmap
-      cr.set_source_rgb(0.0, 0.0, 0.0)
-      cr.set_operator(cairo.OPERATOR_CLEAR)
-      cr.paint()
-
-      # Draw our shape into the bitmap using cairo
-      cr.set_source_rgb(1.0, 1.0, 1.0)
-      cr.set_operator(cairo.OPERATOR_SOURCE)
-      cr.arc(w / 2, h / 2, min(h, w) / 2, 0, 2 * math.pi)
-      cr.fill()
-
-      # Set the window shape
-      win.shape_combine_mask(bitmap, 0, 0)
-
 class PopUp:
     def __init__(self):
         filename = "popup.glade"
@@ -116,19 +96,6 @@ class PopUp:
         #print self.window.get_screen().get_active_window()
         #monitor = self.window.get_screen().get_monitor_geometry(self.window.get_screen().get_monitor_at_window(self.window.get_screen().get_active_window()))
         #print("Heigh: %s, Width: %s" % (monitor.height, monitor.width))
-        
-        self.builder2 = Gtk.Builder()
-        self.builder2.add_from_file("nonclose.glade")
-        self.builder2.connect_signals(self)
-        #self.nwindow = self.builder2.get_object("window1")
-        #self.ntitle = self.builder2.get_object("title")
-        #self.nimage = self.builder2.get_object("image")
-        #self.nwindow.set_keep_above(True)
-        #self.ntitle.set_text("Waiting...")
-        #self.nwindow.show_all()
-        #monitor = self.nwindow.get_screen().get_monitor_geometry(self.nwindow.get_screen().get_monitor_at_window(self.nwindow.get_screen().get_active_window()))
-        #print("Heigh: %s, Width: %s" % (monitor.height, monitor.width))
-        #self.nwindow.move(monitor.width-self.nwindow.get_size()[0],monitor.height-self.nwindow.get_size()[1])
         
         gobject.timeout_add_seconds(1, self.timer)
         gobject.timeout_add_seconds(0.2, self.display_notify)
@@ -185,24 +152,9 @@ class PopUp:
         lispeak.writeSingleInfo("lastid",str(int(message['id'])))
         return True
         
-    def display_notify(self):
-        #if os.path.exists("pycmd_done"):
-        if last_signal==SIG_DONE:
-            #self.ntitle.set_text("Done!")
-            os.system("touch in_grey")
-        #if os.path.exists("pycmd_record"):
-        if last_signal==SIG_RECORD:
-            os.system("touch in_green")
-            #self.ntitle.set_text("Listening...")
-        #if os.path.exists("pycmd_stop"):
-        if last_signal==SIG_STOP:
-            #self.ntitle.set_text("Please Wait...")
-            os.system("touch in_red")
-        #if os.path.exists("pycmd_wait"):
-        if last_signal==SIG_WAIT:
-            #self.ntitle.set_text("Analyzing...")
-            os.system("touch in_progress")
+    def display_notify(self):      
         if os.path.exists("notification.lmf"):          #may use dbus instead of notification.lmf ?
+            print "Notification"
             time.sleep(0.05)
             with open("notification.lmf") as f:
                 data = lispeak.parseData(f.read())
