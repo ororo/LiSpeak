7aadf18ea29cfa06743507c7ef277b3510acd1a0
Luca Vercelli <ororo@email.it>
2014-03-29 17:38:43 +0100

    Fixed error in unicode->str conversion (I hope)

--- Microphone/notify.py
+++ Microphone/notify.py
@@ -60,7 +60,7 @@ class MyDBUSService(dbus.service.Object):
         
     @dbus.service.signal('com.bmandesigns.lispeak.notify')
     def CommandRecognized(self, speech, command):
-        return str(speech),str(command)
+        return speech.encode('utf-8'),command.encode('utf-8')
 
         
 MyDBUSService()
