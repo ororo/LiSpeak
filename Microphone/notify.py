0cb9bd0ad1af13f4bd25b64a25ed3aff4fab64b5
Luca Vercelli <ororo@email.it>
2014-03-29 17:45:26 +0100

    Bug not fixed

--- Microphone/notify.py
+++ Microphone/notify.py
@@ -60,7 +60,8 @@ class MyDBUSService(dbus.service.Object):
         
     @dbus.service.signal('com.bmandesigns.lispeak.notify')
     def CommandRecognized(self, speech, command):
-        return speech.encode('utf-8'),command.encode('utf-8')
+        return str(speech), str(command)
+        #ho to fix??? return speech.encode('utf-8'),command.encode('utf-8')
 
         
 MyDBUSService()
